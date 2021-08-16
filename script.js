5af1d188d7a689fb162e8298ee7b2d63633d602f
Misael Reyes <misaelrey90@gmail.com>
2021-08-15 23:12:01 -0500

    main weather data with search temperature,city,description

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let apiKey = "ae94fa84f4b9b7fef0b6dcc494ea6507"
 let locationEl = document.getElementById("city")
 let inputel = document.getElementById("input")
-let queryUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${inputel.value}&appid=${apiKey}`
+//let queryUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${inputel.value}&appid=${apiKey}`
 
 document.getElementById("searchbtn").addEventListener("click", function (event) {
     event.preventDefault();
@@ -11,5 +11,21 @@ document.getElementById("searchbtn").addEventListener("click", function (event) 
         .then(headers => headers.json())
         .then(weatherData => {
             console.log(weatherData)
+            document.getElementById("location").textContent= weatherData.city.name
+            let lat = weatherData.city.coord.lat;
+            let lon =  weatherData.city.coord.lon;
+            let urlone = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=&appid=${apiKey}`
+            fetch(urlone)
+            .then(headers => headers.json())
+            .then(weatherData =>{
+            console.log(weatherData)
+            document.getElementById("temperture").textContent = Math.floor(weatherData.current.temp*1.8-459.67);
+            document.getElementById("conditons").textContent = weatherData.current.weather[0].description
+            document.getElementById("time").textContent = "1:00"
+            })
         })
 });
+
+
+    
+
